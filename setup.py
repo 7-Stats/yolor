8c1101acf39137f85201ef0e4a59d8d8167abbe7
Damstagram <62882588+Damstagram@users.noreply.github.com>
2022-08-16 21:19:00 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+install_requires=[  'argparse',
+                    'pandas',
+                    'numpy',
+                    'bs4',
+                    'requests']
+
+setup(name='yolor')
